503fdc35ce30e4e1ea30ed94e977a34fbe7df247
Amal031 <amalmj021@gmail.com>
2025-09-02 21:15:08 +0530

    Update anagram.c

--- anagram.c
+++ anagram.c
@@ -1,14 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 void sorting(int a[255], char *s) {
-    for (int i = 0; s[i]; i++)
+    int i = 0;
+    for (i = 0; s[i]; i++)
         a[(unsigned char)s[i]]++;
 }
 
+int len(char *s) {
+    int len = 0;
+    for (len = 0; s[len]; len++);
+    return len;
+}
+
 int anagram(int a[255], int b[255]) {
-    for (int i = 0; i < 255; i++) {
+    int i = 0;
+    for (i = 0; i < 255; i++) {
         if (a[i] != b[i])
             return 0;
     }
@@ -17,25 +24,26 @@ int anagram(int a[255], int b[255]) {
 
 int main(int argc, char **argv) {
     if (argc != 3) {
-        printf("Usage: ./a.out <string1> <string2>\n");
-        return 1;
-    }
-
-    printf("Input: %s %s\n", argv[1], argv[2]);
-
-    if (strlen(argv[1]) != strlen(argv[2])) {
-        printf("Not an anagram .\n");
+        printf("usage: ./a.out string1 string2\n");
         return 0;
     }
 
+    printf("%s %s\n", argv[1], argv[2]);
     int a[255] = {0}, b[255] = {0};
     sorting(a, argv[1]);
     sorting(b, argv[2]);
 
+    int l1 = len(argv[1]);
+    int l2 = len(argv[2]);
+    if (l1 != l2) {
+        printf("not anagram\n");
+        return 0;
+    }
+
     if (anagram(a, b))
-        printf("The strings are anagrams.\n");
+        printf("two strings are anagram\n");
     else
-        printf("The strings are NOT anagrams.\n");
+        printf("not anagram\n");
 
     return 0;
 }
